8d841c852e8b369ee7ada5c0d5c0631be6a0fa93
Pablo-notebook <juarezjpablo7@gmail.com>
2023-09-12 20:56:02 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Sumador.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Common;
+
+namespace Entidades
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+        public Sumador() : this(0)
+        {}
+
+        public long Sumar(long a,long b)
+        {
+            this.cantidadSumas ++;
+            return a + b;
+        }
+
+        public string Sumar(string a,string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador s1) 
+        { 
+            return s1.cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2) 
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return (s1.cantidadSumas == s2.cantidadSumas);
+        }
+    }
+}
---
+++ Sobrecarga-I01/Program.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+namespace Sobrecarga_I01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador1 = new Sumador();
+            string resultadoCadena=sumador1.Sumar("hola","mondo");
+            Console.WriteLine($"Resultado {resultadoCadena}");
+            int conversionExplicitaCantSumas = (int)sumador1;
+            Console.WriteLine($"cant sumas: {conversionExplicitaCantSumas}");
+
+
+        }
+    }
+}
